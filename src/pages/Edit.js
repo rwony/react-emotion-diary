51f98239eafb0ca0338f0f4f7f0c30af6a4f26d0
rwony <88661435+rwony@users.noreply.github.com>
2022-06-24 23:59:03 +0900

    Add files via upload

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,32 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지입니다.</p>
+      <button onClick={() => setSearchParams({ who: "consoleryog" })}>
+        QS 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 이동
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
