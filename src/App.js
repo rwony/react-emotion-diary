1c288870c935a9f0747fe1998a4d916561f0beac
rwony <88661435+rwony@users.noreply.github.com>
2022-06-30 18:34:56 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -36,8 +36,41 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의 일기 1번",
+    date: 1656574351840,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의 일기 2번",
+    date: 1656574351841,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의 일기 3번",
+    date: 1656574351842,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의 일기 4번",
+    date: 1656574351843,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의 일기 5번",
+    date: 1656574351844,
+  },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
   const dataId = useRef(0);
 
   // CREATE
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,68 @@
+import { useContext, useEffect, useState } from "react";
+
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+import DiaryList from "./../components/DiaryList";
+import { DiaryStateContext } from "../App";
+
 const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+
+  // 날짜
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime(); // 이번 년도, 이번 월의 1일의 시간 값을 구함
+
+      // const lastDay = new Date(
+      //   curDate.getFullYear(),
+      //   curDate.getMonth() + 1,
+      //   0
+      // ).getTime(); // 이번 년도, 이번 월의 마지막 날의 시간 값을 구함
+      // 하지만 위와 같은 방법으로 구하면 마지막 일의 00시 00분 00초까지 밖에 못 함
+      // >>> 마지막 일자에 쓴 글은 나타나지 않음
+      // 따라서 아래와 같이 다음 달의 1일의 00시 00분 00초를 구한 후,
+      // 아래 filter 비교문에서 걸러준다.
+
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        1
+      ).getTime(); // 이번 년도, 다음 월의 첫 날의 시간 값을 구함
+
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date < lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>이곳은 홈입니다.</p>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+      <DiaryList diaryList={data} />
     </div>
   );
 };
